08afa46a9a1ff883faea39cdc4e73bdffe52a7bf
musaabullahkhan <musaabullahkhan78@gmail.com>
2025-07-19 13:37:32 +0500

    Practice

--- python2.py
+++ python2.py
@@ -1,1 +1,16 @@
 print("Hello World")
+a = 6 
+b = 4
+print(a+b)
+# 01
+a = 1 # a is an integer
+b = 2.2 # b is floating point number
+c = "Harry" # c is a string
+d = True # d is a boolean(Contains only true or false)
+e = None #e is none(used to assume it have no value)
+# Input
+a = input("Enter Number: 1")
+b = input("Enter Number: 2")
+
+print("Number entered a is: ", a)
+print("Number entered b is: " , b)    
