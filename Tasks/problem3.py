bbd6d5a0b39a7e80de2cb9f14bce23a17008e3e0
musaabullahkhan <musaabullahkhan78@gmail.com>
2025-07-20 17:12:12 +0500

    Practice 2 done

--- Tasks/problem3.py
+++ Tasks/problem3.py
@@ -1,4 +1,27 @@
-a = int(input("Enter number a: "))
-b = int(input("Enter number b: "))
+# 1
+# This script takes two integers as input and prints their sum. 
+a = 3
+b = 2
 
 print("Sum of a and b is:", a + b)
+# 2
+# Remainder 
+a = 64
+b = 12
+
+print("When a is divided by b the remainder is:", a % b)
+# 3
+a = input("Write the value of a")
+# 4
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+print("The number a is greater than b is : ", a > b) 
+# 5
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+print("The average of above two numbers is:", (a+b)/2)
+# 5
+# Squaring 
+a = int(input("Enter your number to find square: "))
+print("Square of the given number is:", a**2 )
